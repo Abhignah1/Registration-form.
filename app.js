a13192d3d6a9dce7836194ef468157edfbdd2016
Abhignah1 <166008560+Abhignah1@users.noreply.github.com>
2024-07-22 16:10:40 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var a = 10;
-var b = 20;
-var c = a + b;
-document.getElementById("id").innerHTML=c;
-
-const pi = 3.142;
-var r = 7;
-document.getElementById("id1").innerHTML="Area of circle: "+pi*r*r;
