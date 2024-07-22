fb40c638133da6664d4112d21e419287920e6f2d
Abhignah1 <166008560+Abhignah1@users.noreply.github.com>
2024-07-22 15:16:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var a = 10;
+var b = 20;
+var c = a + b;
+document.getElementById("id").innerHTML=c;
+
+const pi = 3.142;
+var r = 7;
+document.getElementById("id1").innerHTML="Area of circle: "+pi*r*r;
